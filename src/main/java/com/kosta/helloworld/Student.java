05ef5679f99a08e91d99fc5bab197fb6bfde68a2
existmaster <existmaster@gmail.com>
2020-01-29 17:20:48 +0900

    Student 구조 변경

--- src/main/java/com/kosta/helloworld/Student.java
+++ src/main/java/com/kosta/helloworld/Student.java
@@ -1,20 +1,31 @@
 package com.kosta.helloworld;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "student")
 public class Student {
-    @Id
+
+    @Id @Column @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     @Column
     private String name;
+
     @Column
     private int age;
 
     public void cleaning(){
         System.out.println(name+": Yes, I'm cleaning :(");
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
